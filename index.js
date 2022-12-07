cf72a47013f14b10122d971a291176c21a737929
8bca <111678405+8bca@users.noreply.github.com>
2022-12-07 21:52:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+ var typingEffect = new Typed(".multitext",{
+     strings : ["Website-Desing","Logo-Desing","UI/UX-Desing"] ,
+     loop : true,
+     typeSpeed : 80,
+     backSpeed : 80,
+     backDelay : 1500
+     
+ })
